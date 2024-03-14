9f840a8060bdc8e8260fa5aa1fdad8bbc479d34d
Ukonu Lucky Ifeanyichukwu <48377220+ukonulucky@users.noreply.github.com>
2024-03-14 11:24:58 +0100

    auth

--- Middleware/isAuthenticated.js
+++ Middleware/isAuthenticated.js
@@ -1,14 +1,9 @@
 const expressAsyncHandler = require('express-async-handler')
-const { Magic } = require('@magic-sdk/admin');
 const { validateJwt } = require('../utils/jwt');
 const UserModel = require('../models/user');
 
-
 const isAuthenticated = expressAsyncHandler(async (req, res, next) => {
    const {email} = req.body
-   console.log(email)
-   const getAllUsers = await UserModel.find()
-   console.log(getAllUsers)
    const foundUser = await UserModel.findOne({email})
    console.log(foundUser)
    if(!foundUser){
--- routes/createContestRoute.js
+++ routes/createContestRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { createContestController, findSkillGapContestcrontrolelr } = require('../controllers/createContestController');
+const isAuthenticated = require('../Middleware/isAuthenticated');
 
 
 const createContestRouter = express.Router();
 
 
-createContestRouter.post("/create-contest", createContestController)
+createContestRouter.post("/create-contest",isAuthenticated, createContestController)
 
 createContestRouter.get("/find-skillGap", findSkillGapContestcrontrolelr )
 
